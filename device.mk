5610528e509b7e597d8ef3ec26008d45fb4ba059
realahnet <syedmusicrealise@gmail.com>
2026-03-03 02:46:22 +0800

    a52sxq: Ship dax config for dolby
    * Values adapted to sony's dolby stack

--- device.mk
+++ device.mk
@@ -31,6 +31,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml \
+    $(LOCAL_PATH)/configs/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Sensors
 PRODUCT_COPY_FILES += \
